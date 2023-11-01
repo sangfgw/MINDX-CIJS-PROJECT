1a2e23ad9877d0bc2039d54ceb39a2f209cd0220
sangfgw <sang050901i@gmail.com>
2023-11-01 11:41:30 +0700

    Updates

--- src/utils/generate/randomVideoId.js
+++
@@ -1,9 +0,0 @@
-// Func: generate random video trailer
-export const generateRandomTrailerVideoId = (videos) => {
-  if (!videos || !videos.length > 0) return;
-  // console.log(videos);
-  const randomIndex = Math.floor(Math.random() * (videos.length - 1));
-
-  // console.log(randomIndex);
-  return videos[randomIndex]?.key;
-};
